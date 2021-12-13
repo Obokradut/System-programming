b0a1891946c1dead2482f74b2622ebc052a4f9e2
Obokradut <77218722+Obokradut@users.noreply.github.com>
2021-12-13 09:55:09 +0300

    Написал конкатенацию на ассемблере

---
+++ ASM_2/ASM_2/ASM_2.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    char s[1024];
+    char str1[] = "test";
+    char str2[] = "_13qwer";
+    char f[] = "%s";
+    _asm
+    {
+        lea esi, [str1]
+        lea edi, [s]
+
+        FOR_1:
+        lodsb
+            stosb
+            cmp al, 0
+            jnz FOR_1
+
+            lea esi, [str2]
+            dec edi
+
+            FOR_2 :
+        lodsb
+            stosb
+            cmp al, 0
+            jnz FOR_2
+    }
+    std::cout << s;
+}
